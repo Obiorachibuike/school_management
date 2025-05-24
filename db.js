1711f6bc5647ef9c41226ede703428cc7eee45f3
Chibuike Obiora <104082031+Obiorachibuike@users.noreply.github.com>
2025-05-24 21:19:34 +0100

    Update db.js

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // Add port from .env
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -18,8 +19,8 @@ const pool = mysql.createPool({
     console.log('Connected to MySQL database (pool)');
     connection.release();
   } catch (err) {
-    console.error('MySQL pool connection error:', err);
-    process.exit(1); // exit on failure
+    console.error('MySQL pool connection error:', err.message);
+    process.exit(1); // Exit app if DB connection fails
   }
 })();
 
