6dae98ec8c4a32a086b3f1a257a969e0a243c568
Chibuike Obiora <104082031+Obiorachibuike@users.noreply.github.com>
2025-05-24 19:49:00 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,19 +1,23 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect(err => {
+pool.getConnection((err, connection) => {
   if (err) {
-    console.error('MySQL connection error:', err);
+    console.error('MySQL pool connection error:', err);
     throw err;
   }
-  console.log('Connected to MySQL database');
+  console.log('Connected to MySQL database (pool)');
+  connection.release(); // release the connection back to the pool
 });
 
-module.exports = db;
+module.exports = pool.promise();
